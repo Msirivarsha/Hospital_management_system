6c027b3bdad85c28bdb7d53bfaedcb42c7e2a759
M Sirivarsha <sirivarsha1862004@gmail.com>
2025-10-17 15:04:56 +0530

    Delete java/com/hospital_management_system/demo directory

--- java/com/hospital_management_system/demo/repository/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hospital_management_system.demo.repository;
-
-import com.hospital_management_system.demo.entity.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
-}
--- java/com/hospital_management_system/demo/repository/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hospital_management_system.demo.repository;
-
-
-import com.hospital_management_system.demo.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Integer> {}
-
