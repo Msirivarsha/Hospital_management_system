dd030d3957adb67c4eaf6a04f24f5bffe5b14efb
M Sirivarsha <sirivarsha1862004@gmail.com>
2025-10-17 15:04:09 +0530

    Delete sep14 patient_dashboard directory

--- sep14 patient_dashboard/java/com/hospital_management_system/demo/repository/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hospital_management_system.demo.repository;
-
-import com.hospital_management_system.demo.entity.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillRepository extends JpaRepository<Bill, Integer> {
-}
